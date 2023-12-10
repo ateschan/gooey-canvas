de5701a965baaa850f3a6c714b11410d20cbe505
ateschan <addisontescahn@gmail.com>
2023-12-10 15:49:32 -0600

    refactor this joint

--- gooey-canvas/src/lib/Greet.svelte
+++ gooey-canvas/src/lib/Greet.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri"
 
-  let key = "";
+  let name = "";
   let greetMsg = ""
 
   async function greet(){
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { key })
+    greetMsg = await invoke("greet", { name })
   }
 </script>
 
 <div>
   <form class="row" on:submit|preventDefault={greet}>
     <input id="greet-input" placeholder="Enter an API Key..." bind:value={name} />
-    <button type="submit">Submit</button>
+    <button type="submit">Greet</button>
   </form>
   <p>{greetMsg}</p>
 </div>
