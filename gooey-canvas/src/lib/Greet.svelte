ceed5798658c0254e9807eee2e91459e1b1d7d06
ateschan <addisontescahn@gmail.com>
2023-12-10 15:03:42 -0600

    changed defaults

--- gooey-canvas/src/lib/Greet.svelte
+++ gooey-canvas/src/lib/Greet.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri"
 
-  let name = "";
+  let key = "";
   let greetMsg = ""
 
   async function greet(){
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+    greetMsg = await invoke("greet", { key })
   }
 </script>
 
 <div>
   <form class="row" on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
+    <input id="greet-input" placeholder="Enter an API Key..." bind:value={name} />
+    <button type="submit">Submit</button>
   </form>
   <p>{greetMsg}</p>
 </div>
