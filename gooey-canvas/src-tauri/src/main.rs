22caa3cf0ba29b4f4928e77b4c8e7287ef12ed24
ateschan <addisontescahn@gmail.com>
2023-12-12 00:38:26 -0600

    Scaffolding api endpoints

--- gooey-canvas/src-tauri/src/main.rs
+++ gooey-canvas/src-tauri/src/main.rs
@@ -4,6 +4,8 @@ mod canvas;
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+//set api key
 fn set_apikey(key: &str) {
     env::set_var("CANVAS_API_KEY", key);
 }
@@ -21,16 +23,33 @@ fn greet(key: &str) -> String {
     let user = canvas::get_user(key);
     let name: &str = &user.name;
     set_apikey(key);
-    println!("{}", get_user_courses());
     format!("Hello from rust! Your name is {}", name)
 }
 
-//returns a json string of the user's courses
 #[tauri::command]
-fn get_user_courses() -> String {
+fn get_course_assignments(id: i32) -> serde_json::Value {
+    let key = &canvas::get_apikey();
+    let assignments = canvas::retrieve_assignments(key, id);
+    let json = serde_json::to_string(&assignments).unwrap();
+    return serde_json::from_str(&json).unwrap();
+}
+
+//returns a json string of the user's courses
+//json is bare right now TODO make a struct for it and simplfy this redundant code
+#[tauri::command]
+fn get_user_courses() -> serde_json::Value {
     let joint = &canvas::get_apikey();
     let courses = canvas::get_courses(joint);
-    format!("{}", courses)
+    let json: serde_json::Value = serde_json::from_str(&courses).unwrap();
+    json //return json string
+}
+
+#[tauri::command]
+fn get_user() -> serde_json::Value {
+    let key = &canvas::get_apikey();
+    let user = &canvas::get_user(key);
+    let json = serde_json::to_string(&user).unwrap();
+    return serde_json::from_str(&json).unwrap();
 }
 
 //generate handlers for typescript
