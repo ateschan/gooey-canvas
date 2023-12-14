ea5c426b41c33c2c9c406c7f375a172da60d304a
ateschan <addisontescahn@gmail.com>
2023-12-14 12:41:13 -0600

    Backend Update
    *tests not included

--- gooey-canvas/src-tauri/src/tests.rs
+++ gooey-canvas/src-tauri/src/tests.rs
@@ -5,7 +5,7 @@ pub fn test_canvas_functions() {
     let courses = canvas::get_courses(user.id);
     let mut assignments = canvas::get_assignments(1567838);
     for assignment in &mut assignments {
-        match assignment.grade {
+        match &assignment.letter_grade {
             Some(grade) => println!("Grade: {}", grade),
             None => println!("No grade"),
         }
